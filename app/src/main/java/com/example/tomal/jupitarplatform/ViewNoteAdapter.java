5aa34dc65a3acc0d87590091e6e28fce972ddb54
Omar Khaium Chowdhury <omar.piistech@gmail.com>
2019-03-25 00:21:13 +0600

    gfgffgggfdg

--- app/src/main/java/com/example/tomal/jupitarplatform/ViewNoteAdapter.java
+++ app/src/main/java/com/example/tomal/jupitarplatform/ViewNoteAdapter.java
@@ -3,41 +3,55 @@ package com.example.tomal.jupitarplatform;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import org.emptybit.help.Format;
+
 import java.util.ArrayList;
 
-public class ViewNoteAdapter extends ArrayAdapter<NoteModel> {
+public class ViewNoteAdapter extends RecyclerView.Adapter<ViewNoteAdapter.ViewHolder> {
 
     private Context context;
     private ArrayList<NoteModel> arrayList;
 
-    public ViewNoteAdapter(@NonNull Context context, ArrayList<NoteModel> array) {
-        super(context, R.layout.row_layout, array);
+    public ViewNoteAdapter(Context context, ArrayList<NoteModel> arrayList) {
         this.context = context;
-        this.arrayList = array;
+        this.arrayList = arrayList;
     }
     @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+    public ViewNoteAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.view_note_row_layout, viewGroup, false);
+        ViewNoteAdapter.ViewHolder holder = new ViewNoteAdapter.ViewHolder(view);
+        return holder;
+    }
+    @Override
+    public void onBindViewHolder(@NonNull ViewNoteAdapter.ViewHolder viewHolder, int position) {
 
-        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        convertView = inflater.inflate(R.layout.view_note_row_layout, parent, false);
 
-        TextView xName = convertView.findViewById(R.id.view_notes_row_name);
-        TextView xDate = convertView.findViewById(R.id.view_notes_row_date);
-        TextView xNote = convertView.findViewById(R.id.view_notes_row_note);
-
-        xName.setText(arrayList.get(position).getName());
-        xDate.setText(arrayList.get(position).getDate());
-        xNote.setText(arrayList.get(position).getNote());
-
-        return convertView;
+        viewHolder.xName.setText(Format.Text(arrayList.get(position).getName()));
+        viewHolder.xDate.setText(Format.Date(arrayList.get(position).getDate()));
+        viewHolder.xNote.setText(Format.Text(arrayList.get(position).getNote()));
     }
 
+    @Override
+    public int getItemCount() {
+        return arrayList.size();
+    }
 
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView xName, xDate, xNote;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            xName = itemView.findViewById(R.id.view_notes_row_name);
+            xDate = itemView.findViewById(R.id.view_notes_row_date);
+            xNote = itemView.findViewById(R.id.view_notes_row_note);
+        }
+    }
 }
--- app/src/main/java/com/example/tomal/jupitarplatform/ViewNotesActivity.java
+++ app/src/main/java/com/example/tomal/jupitarplatform/ViewNotesActivity.java
@@ -1,8 +1,12 @@
 package com.example.tomal.jupitarplatform;
 
 import android.content.Intent;
+import android.os.Build;
+import android.os.Handler;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
 import android.view.View;
@@ -10,15 +14,29 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import static com.example.tomal.jupitarplatform.MainActivity.COOKIE_FOR_API;
+import static com.example.tomal.jupitarplatform.MainActivity.LEAD_ID;
 
 public class ViewNotesActivity extends AppCompatActivity {
 
-    ListView xListView;
-    ProgressBar xProgressBar;
+    RecyclerView xListView;
+    //ProgressBar xProgressBar;
     ViewNoteAdapter adapter;
     ArrayList<NoteModel> noteModels = new ArrayList<>();
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,8 +50,10 @@ public class ViewNotesActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
+
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+        xListView = findViewById(R.id.view_notes_list);
 
         Button backButton = findViewById(R.id.back_button);
         backButton.setOnClickListener(new View.OnClickListener() {
@@ -44,19 +64,7 @@ public class ViewNotesActivity extends AppCompatActivity {
             }
         });
 
-        xListView = findViewById(R.id.view_notes_list);
-        xProgressBar = findViewById(R.id.view_notes_progress);
-
-        noteModels.add(new NoteModel("", "Alan", "24-Nov-2018", "Dummy Note 1"));
-        noteModels.add(new NoteModel("", "Jon", "23-Nov-2018", "Dummy Note 2"));
-        noteModels.add(new NoteModel("", "Bob", "25-Nov-2018", "Dummy Note 3"));
-        noteModels.add(new NoteModel("", "Alice", "26-Nov-2018", "Dummy Note 4"));
-        noteModels.add(new NoteModel("", "Marsh", "27-Nov-2018", "Dummy Note 5"));
-
-        adapter = new ViewNoteAdapter(this, noteModels);
-        xListView.setAdapter(adapter);
-        xListView.setVisibility(View.VISIBLE);
-        xProgressBar.setVisibility(View.GONE);
+        getData();
     }
 
     @Override
@@ -68,4 +76,68 @@ public class ViewNotesActivity extends AppCompatActivity {
         startService(new Intent(getApplicationContext(), BackgroundService.class));
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    private void getData() {
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://jupiter.centralstationmarketing.com/api/getnotes.php")
+                .get()
+                .addHeader("Cookie", COOKIE_FOR_API)
+                .addHeader("leadid", LEAD_ID)
+                .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                .addHeader("cache-control", "no-cache")
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+
+            Handler mainHandler = new Handler(getApplicationContext().getMainLooper());
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                final String myResponse = response.body().string();
+
+                mainHandler.post(new Runnable() {
+                    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+                    @Override
+                    public void run() {
+                        try {
+                            JSONObject json = new JSONObject(myResponse);
+                            System.out.println(json.toString());
+                            JSONArray jsonResult = json.getJSONArray("Notes");
+                            if (jsonResult != null) {
+                               // xNoDataFound.setVisibility(View.GONE);
+                                for (int i = jsonResult.length() - 1; i >= 0; i--) {
+                                    JSONObject jsonObject = jsonResult.getJSONObject(i);
+
+                                    noteModels.add(new NoteModel(
+                                                    jsonObject.getString("id"),
+                                                    jsonObject.getString("u_fullname"),
+                                                    jsonObject.getString("timestamp"),
+                                                    jsonObject.getString("comment")
+
+                                            )
+
+                                    );
+                                }
+
+                                adapter = new ViewNoteAdapter(getApplicationContext(), noteModels);
+                                xListView.setAdapter(adapter);
+
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+
 }
